a1212b8bddcf70333e6da7ecbe2d0913cd6571aa
Thanainun <ctodui@gmail.com>
2014-02-13 04:04:33 +0700

    Etc update
    Etc update

---
+++ etc/chilliclear.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -Rf /var/run/chilli.*
+rm -Rf /tmp/*
---
+++ etc/phpmyadmin/config-db.php
@@ -0,0 +1,19 @@
+<?php
+##
+## database access settings in php format
+## automatically generated from /etc/dbconfig-common/phpmyadmin.conf
+## by /usr/sbin/dbconfig-generate-include
+## Fri, 31 Jan 2014 01:38:26 +0700
+##
+## by default this file is managed via ucf, so you shouldn't have to
+## worry about manual changes being silently discarded.  *however*,
+## you'll probably also want to edit the configuration file mentioned
+## above too.
+##
+$dbuser='phpmyadmin';
+$dbpass='duydui1909';
+$basepath='';
+$dbname='phpmyadmin';
+$dbserver='';
+$dbport='';
+$dbtype='mysql';
